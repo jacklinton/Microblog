8acbb0fe7d57181732cbbae100d181ace9bb623c
Jack Linton <jacklinton@mac.com>
2017-02-16 17:23:15 -0500

    Some simple modifications to observe changes.

--- main.rb
+++ main.rb
@@ -1,8 +1,16 @@
 require "sinatra"
 require "sinatra/activerecord"
 require "./models"
+require "gon-sinatra"
+require 'rabl'
+require 'active_support/core_ext'
+require 'active_support/inflector'
+require 'builder'
 
+Rabl.register!
 Sinatra::register Gon::Sinatra::Rabl
+
+
 
 set :database, "/////"
 
